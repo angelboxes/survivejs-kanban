25a6d4dc93e589945f7315a5357fbc31e8ed90a2
angelboxes <angel.cj.o@gmail.com>
2017-09-06 14:58:45 -0600

    Notes now can be dragged from one lane to another

--- app/actions/LaneActions.js
+++ app/actions/LaneActions.js
@@ -1,3 +1,5 @@
 import alt from '../libs/alt';
 export default alt.generateActions(
-	'create', 'update', 'delete', 'clear', 'attachToLane', 'detachFromLane');
+	'create', 'update', 'delete', 'clear', 
+	'attachToLane', 'detachFromLane',
+	'move');
