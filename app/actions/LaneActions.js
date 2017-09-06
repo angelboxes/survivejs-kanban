6f2b1e299ecf84dca4fd4f5abd7fc1d43a452307
angelboxes <angel.cj.o@gmail.com>
2017-09-06 10:53:10 -0600

    Lanes now can be removed or edited

--- app/actions/LaneActions.js
+++ app/actions/LaneActions.js
@@ -1,3 +1,3 @@
 import alt from '../libs/alt';
 export default alt.generateActions(
-	'create','clear', 'attachToLane', 'detachFromLane');
+	'create', 'update', 'delete', 'clear', 'attachToLane', 'detachFromLane');
--- app/stores/LaneStore.js
+++ app/stores/LaneStore.js
@@ -40,4 +40,21 @@ export default class LaneStore {
 			})
 		});
 	}
+
+	update(updatedLane){
+		this.setState({
+			lanes: this.lanes.map(lane=> {
+				if(lane.id === updatedLane.id){
+					return Object.assign({}, lane, updatedLane);
+				}
+				return lane;
+			})
+		});
+	}
+
+	delete(id) {
+		this.setState({
+			lanes: this.lanes.filter(lane => lane.id !== id)
+		});
+	}
 }
